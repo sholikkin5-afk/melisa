4c15bd9548a5806bd3fb948a4a188a6680e69be4
sholikkin5-afk <sholikkin5@gmail.com>
2026-08-26 13:55:50 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('melisa-v1').then(cache=>{return cache.addAll(['/','/logo.jpg']);}));});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(response=>response||fetch(e.request)));});
